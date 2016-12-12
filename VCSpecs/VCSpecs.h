1e5bc93b8099c6f1a530ba19342fea356738d362
Randall Brown <blladnar@gmail.com>
2016-12-11 22:55:14 -0800

    Initial Commit

---
+++ VCSpecs/VCSpecs.h
@@ -0,0 +1,19 @@
+//
+//  VCSpecs.h
+//  VCSpecs
+//
+//  Created by Randall Brown on 12/11/16.
+//  Copyright © 2016 Randall Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VCSpecs.
+FOUNDATION_EXPORT double VCSpecsVersionNumber;
+
+//! Project version string for VCSpecs.
+FOUNDATION_EXPORT const unsigned char VCSpecsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCSpecs/PublicHeader.h>
+
+
